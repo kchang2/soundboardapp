174f68b589f0bfda554a46cd064696f8d9c9a5be
Chang <Kai-TangC@Dodgers.LADodgers.com>
2017-09-28 05:22:58 -0700

    Begin ML on facial recognition

---
+++ face_recognition.py
@@ -0,0 +1,19 @@
+import cv2
+import os
+import numpy as np
+
+"""
+This is the structure of the repository
+
+data
+|------- train
+|           |----- kaichang1.jpg
+|           |----- kaichang2.jpg
+|           |----- ...
+|           |----- kaichangN.jpg
+|------- test
+|           |----- test1.jpg
+|           |----- test2.jpg
+|           |----- test3.jpg
+
+"""
